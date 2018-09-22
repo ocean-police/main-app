fd92ddb6586e7683d707cff093561c41b5757856
Michael Englo <menglo@optimalefficiency.net>
2018-09-22 14:25:07 -0700

    make some mock data in redux  store

--- ocean-police/src/reducers/clothings.js
+++ ocean-police/src/reducers/clothings.js
@@ -1,4 +1,34 @@
 const seed = {
+  '001': {
+    name: 'my red Tees',
+    type: 'T_SHIRT',
+    materials: [
+      {
+        type: 'POLYESTER',
+        percentage: 50,
+      },
+      {
+        type: 'COTTON',
+        percentage: 25,
+      },
+      {
+        type: 'METAL',
+        percentage: 25,
+      },
+    ],
+    washingFrequency: 1,
+  },
+  '002': {
+    name: 'Jacket #1',
+    type: 'JACKET',
+    materials: [
+      {
+        type: 'Leather',
+        percentage: 100,
+      },
+    ],
+    washingFrequency: 3,
+  },
 };
 
 const clothings = (state = seed, action) => {
