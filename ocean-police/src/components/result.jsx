1ec011824bb4f2feab5215cf65e64eaccc0357bc
Won Hong <whong@outlook.com>
2018-09-22 14:53:50 -0700

    adding donught chart

--- ocean-police/src/components/result.jsx
+++ ocean-police/src/components/result.jsx
@@ -1,6 +1,27 @@
 import React from 'react';
+import { Chart, Axis, Series, Tooltip, Cursor, Line, Doughnut, Pie } from "react-charts";
 
 export default function Result () {
-  return <h1>this is result page</h1>;
+
+  var data = [
+    {
+      label: "Series 1",
+      data: [[0, 1], [1, 2], [2, 4], [3, 2], [4, 7]]
+    }
+  ]
+
+
+  return <div className="">
+    <h1>this is result page</h1>
+
+    <div className="chartArea">
+      <Chart data={data}>
+        <Axis type="pie" />
+        <Series type={Pie} showPoints={false} />
+        <Tooltip />
+      </Chart>
+    </div>
+
+  </div>
 }
 
