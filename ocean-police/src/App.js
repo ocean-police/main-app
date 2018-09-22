f381643dbad0f17e195f907c529cdfa00001d677
Won Hong <whong@outlook.com>
2018-09-22 12:05:36 -0700

    setsup the router

--- ocean-police/src/App.js
+++ ocean-police/src/App.js
@@ -1,21 +1,44 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import './App.css';
+import Input from "./components/input";
+import Result from "./components/result";
 
-class App extends Component {
+export default class App extends Component {
+
+  constructor(props){
+    super(props);
+
+  }
+  goToInputPage() {
+    return <Input/>
+  }
+
+  goToResultPage() {
+    return <Result/>;
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo"/>
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <BrowserRouter>
+          <div id="application-container">
+            <Switch>
+              <Route exact path="/" component={this.goToInputPage}/>
+              <Route exact path="/result" component={this.goToResultPage}/>
+            </Switch>
+          </div>
+        </BrowserRouter>;
       </div>
+
     );
   }
 }
-
-export default App;
---
+++ ocean-police/src/components/input.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+export default function Input () {
+  return <h1>this is input page</h1>;
+}
+
