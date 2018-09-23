81aa3e6318f5219a29acfe7ad307b354d1eb1166
Michael Englo <menglo@optimalefficiency.net>
2018-09-22 20:59:03 -0700

    complete the add page

--- ocean-police/src/containers/AddClothingPage.js
+++ ocean-police/src/containers/AddClothingPage.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import GarmentOption from '../components/GarmentOption';
-import { Button, Typography, TextField, withStyles, Grid, InputAdornment, FormControl, InputLabel, Input } from '@material-ui/core';
+import { Button, Typography, withStyles, Grid, InputAdornment, Input, MenuItem } from '@material-ui/core';
 import Search from '@material-ui/icons/Search';
+import Select from '@material-ui/core/Select';
 
 // import { diff } from 'deep-object-diff';
 
 const styles = {
   container: {
     margin: "5vw"
+  },
+  smallCaption: {
+    textAlign: "center",
   },
   searchSection: {
     fontSize: "40px",
@@ -18,7 +22,10 @@ const styles = {
     width: "95%",
     margin: "0 2.5%",
     backgroundColor: "#7A98E7"
-  }
+  },
+  washingPeriodSelect: {
+    marginBottom: '20px',
+  },
 };
 
 class AddClothingPage extends Component {
@@ -43,13 +50,11 @@ class AddClothingPage extends Component {
     const { classes } = this.props;
     return (
       <div className={classes.container}>
-        <Grid container className={classes.root}>
-          <Grid item xs={12}>
+        <Grid container className={classes.root} alignItems="center" spacing={16}>
+          <Grid item xs={12} className="direction-type-1 full-width">
             <Typography variant="display1">
               Garment Type
             </Typography>
-          </Grid>
-          <Grid item xs={12} className="direction-type-1 full-width">
             <Typography variant="caption">
               Start your entry with garment type
             </Typography>
@@ -67,7 +72,7 @@ class AddClothingPage extends Component {
             />
           </Grid>
 
-          <Grid container>
+          <Grid container alignContent="center">
             <Grid item xs={3}>
               <GarmentOption />
             </Grid>
@@ -92,6 +97,51 @@ class AddClothingPage extends Component {
             <Grid item xs={3}>
               <GarmentOption />
             </Grid>
+          </Grid>
+
+          <Grid item xs={12}>
+            <Typography variant="display1">
+              Material Composition
+            </Typography>
+            <Typography variant="caption">
+              Take a picture of the tag that contains the material composition
+            </Typography>
+          </Grid>
+
+          <Grid item xs={5} />
+          <Grid item xs={7}>
+            <GarmentOption/>
+          </Grid>
+
+          <Grid item xs={12} className={classes.smallCaption}>
+            <Typography variant="caption">Or input your computer</Typography>
+          </Grid>
+
+          <Grid item xs={12}>
+            <Typography variant="display1">
+              Washing Habits
+            </Typography>
+            <Typography variant="caption">
+              Let's see how often will you be washing this garment.
+            </Typography>
+          </Grid>
+
+          <Grid item xs={12}>
+            <Select
+                value="every 1 week"
+                onChange={() => {}}
+                fullWidth
+                className={classes.washingPeriodSelect}
+            >
+              <MenuItem value="">
+                <em>None</em>
+              </MenuItem>
+              <MenuItem value="every 1 week">Every 1 Week</MenuItem>
+              <MenuItem value="every 2 weeks">Every 2 Weeks</MenuItem>
+              <MenuItem value="every 3 weeks">Every 3 Weeks</MenuItem>
+              <MenuItem value="a month">A Month</MenuItem>
+              <MenuItem value="customs">Custom</MenuItem>
+            </Select>
           </Grid>
 
           <Grid item xs={1} />
